dc71a8e5e656e5da1f30e5799e7987a7e85dfb0a
Alex Nichol <unixpickle@gmail.com>
2015-10-27 20:18:48 -0400

    use brute force test

--- test/bin_dfa/core_test.clj
+++ test/bin_dfa/core_test.clj
@@ -4,13 +4,16 @@
 
 (deftest run-machine-test
   (letfn [(accepting [state] (or (= state "AD") (= state "ABD")))
-          (not-accepting [state] (not (accepting state)))]
+          (not-accepting [state] (not (accepting state)))
+          (all-inputs [length] (if (zero? length)
+              '(())
+              (apply concat (for [x [0 1]] (map (partial cons x) (all-inputs (- length 1)))))))
+          (suffix [sequence] (if (= (count sequence) 3) sequence (suffix (rest sequence))))
+          (should-accept [sequence] (let [s (suffix sequence)]
+              (or (= '(0 1 0) s) (= '(0 1 1) s))))]
     (testing "run-machine"
       (testing "accept strings ending in 010 or 011"
         (let [machine (process-machine
               '((A AB A) (AB AB AC) (AC ABD AD) (ABD AB AC) (AD AB A)))]
-          (is (accepting (run-machine machine "A" (parse-input "000111011"))))
-          (is (accepting (run-machine machine "A" (parse-input "000111010"))))
-          (is (accepting (run-machine machine "A" (parse-input "00011101010"))))
-          (is (not-accepting (run-machine machine "A" (parse-input "0001110101"))))
-)))))
+          (doseq [input (all-inputs 8)] (let [result (run-machine machine "A" input)]
+              (is (= (accepting result) (should-accept input)) input))))))))
